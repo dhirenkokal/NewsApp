3daa20e892a7a416e7f468202ca9dbc7e8cba8af
dhirenkokal <dhirenkokal6@gmail.com>
2024-03-13 19:39:22 +0530

    First Commit with SplashScreen

---
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.newsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.newsapp.ui.theme.NewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContent {
+            NewsAppTheme {
+
+            }
+        }
+    }
+}
